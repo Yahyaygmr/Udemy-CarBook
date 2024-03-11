eef8b4a050292eea0148add2734c2203874f0f0e
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-03-11 12:43:48 +0300

    CQRS for brand cotrollers and di container

--- Presentation/CarBook.WebApi/Program.cs
+++ Presentation/CarBook.WebApi/Program.cs
@@ -2,6 +2,8 @@ using CarBook.Application.Features.CQRS.Handlers.AboutHandlers.Read;
 using CarBook.Application.Features.CQRS.Handlers.AboutHandlers.Write;
 using CarBook.Application.Features.CQRS.Handlers.BannerHandlers.Read;
 using CarBook.Application.Features.CQRS.Handlers.BannerHandlers.Write;
+using CarBook.Application.Features.CQRS.Handlers.BrandHandlers.Read;
+using CarBook.Application.Features.CQRS.Handlers.BrandHandlers.Write;
 using CarBook.Application.Interfaces;
 using CarBook.Persistence.Context;
 using CarBook.Persistence.Repositories;
@@ -25,6 +27,13 @@ builder.Services.AddScoped<GetBannerQueryHandler>();
 builder.Services.AddScoped<CreateBannerCommandHandler>();
 builder.Services.AddScoped<RemoveBannerCommandHandler>();
 builder.Services.AddScoped<UpdateBannerCommandHandler>();
+
+builder.Services.AddScoped<GetBrandByIdQueryHandler>();
+builder.Services.AddScoped<GetBrandQueryHandler>();
+builder.Services.AddScoped<CreateBrandCommandHandler>();
+builder.Services.AddScoped<RemoveBrandCommandHandler>();
+builder.Services.AddScoped<UpdateBrandCommandHandler>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

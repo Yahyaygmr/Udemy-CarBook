9d1e341b4c0e81fa7f5563883666f85ca2ad8300
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-03-25 18:25:06 +0300

    committed

---
+++ Infrastructure/CarBook.Persistence/Repositories/CarPricingRepositories/CarPricingRepository.cs
@@ -0,0 +1,43 @@
+﻿using CarBook.Application.Interfaces.CarInterfaces;
+using CarBook.Application.Interfaces.CarPricingInterfaces;
+using CarBook.Domain.Entities;
+using CarBook.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Persistence.Repositories.CarPricingRepositories
+{
+    public class CarPricingRepository : ICarPricingRepository
+    {
+        private readonly CarBookContext _context;
+
+        public CarPricingRepository(CarBookContext context)
+        {
+            _context = context;
+        }
+
+        public List<CarPricing> GetCarPricingWithCars()
+        {
+            var values = _context.CarPricings
+                .Include(c => c.Car)
+                .ThenInclude(c => c.Brand)
+                .Include(c => c.Pricing)
+                .ToList();
+            return values;
+        }
+        public List<CarPricing> GetCarPricingWithCarsDaily()
+        {
+            var values = _context.CarPricings
+                .Include(c => c.Car)
+                .ThenInclude(c => c.Brand)
+                .Include(c => c.Pricing)
+                .Where(x => x.PricingId == 3)
+                .ToList();
+            return values;
+        }
+    }
+}
---
+++ Presentation/CarBook.WebApi/Controllers/CarPricingsController.cs
@@ -0,0 +1,31 @@
+﻿using CarBook.Application.Features.Mediator.Queries.CarPricingQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarPricingsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CarPricingsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet("GetCarPricingWithCaListDaily")]
+        public async Task<IActionResult> GetCarPricingWithCaListDaily()
+        {
+            var values = await _mediator.Send(new GetCarPricingWithCarQuery());
+            return Ok(values);
+        }
+        //[HttpGet("GetCarPricingWithCaList")]
+        //public async Task<IActionResult> GetCarPricingWithCaList()
+        //{
+        //    var values = await _mediator.Send(new GetCarPricingWithCarQuery());
+        //    return Ok(values);
+        //}
+    }
+}

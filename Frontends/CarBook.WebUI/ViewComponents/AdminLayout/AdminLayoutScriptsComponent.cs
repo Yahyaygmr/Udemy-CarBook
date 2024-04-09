0544b5d7d8022b8e63b93ea3f3d2df69951e709d
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-04-09 15:06:14 +0300

    created admin layout

---
+++ Frontends/CarBook.WebUI/ViewComponents/AdminLayout/AdminLayoutScriptsComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.ViewComponents.AdminLayout
+{
+    public class AdminLayoutScriptsComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}

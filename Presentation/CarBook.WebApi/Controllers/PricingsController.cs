0c35a42cb6eb679a0db4fd5a1ba756dd3ea69ef0
Yahyaygmr <yahyaygmr@gmail.com>
2024-04-16 11:07:00 +0300

    admin pricing

--- Presentation/CarBook.WebApi/Controllers/PricingsController.cs
+++ Presentation/CarBook.WebApi/Controllers/PricingsController.cs
@@ -40,7 +40,7 @@ namespace CarBook.WebApi.Controllers
             await _mediator.Send(command);
             return Ok("Ödeme periyodu başarıyla güncellendi.");
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> RemovePricing(int id)
         {
             await _mediator.Send(new RemovePricingCommand(id));

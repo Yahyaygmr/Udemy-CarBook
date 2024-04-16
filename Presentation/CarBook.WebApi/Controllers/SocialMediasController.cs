2b1d82e3bae1d9be58c73edf6cb2b448deaf7b99
Yahyaygmr <yahyaygmr@gmail.com>
2024-04-16 20:49:53 +0300

    admin social media

--- Presentation/CarBook.WebApi/Controllers/SocialMediasController.cs
+++ Presentation/CarBook.WebApi/Controllers/SocialMediasController.cs
@@ -40,7 +40,7 @@ namespace CarBook.WebApi.Controllers
             await _mediator.Send(command);
             return Ok("Sosyal medya hesabı başarıyla güncellendi.");
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> RemoveSocialMedia(int id)
         {
             await _mediator.Send(new RemoveSocialMediaCommand(id));

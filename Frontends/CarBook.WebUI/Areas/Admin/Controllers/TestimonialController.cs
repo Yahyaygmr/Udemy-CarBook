f01828069fd9cb29c170c1b7b64c78ac60927a9e
Yahyaygmr <yahyaygmr@gmail.com>
2024-04-16 21:25:32 +0300

    admin testimonial

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/TestimonialController.cs
@@ -0,0 +1,90 @@
+﻿using CarBook.Dto.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/{controller}/{action}/{id?}")]
+    public class TestimonialController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TestimonialController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44386/api/Testimonials");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateTestimonial()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateTestimonial(CreateTestimonialDto dto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(dto);
+
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44386/api/Testimonials", content);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Testimonial");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateTestimonial(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:44386/api/Testimonials/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateTestimonialDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateTestimonial(UpdateTestimonialDto dto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(dto);
+
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:44386/api/Testimonials/", content);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Testimonial");
+            }
+            return View();
+        }
+        public async Task<IActionResult> RemoveTestimonial(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:44386/api/Testimonials/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Testimonial");
+            }
+            return View();
+        }
+    }
+}
--- Presentation/CarBook.WebApi/Controllers/TestimonialsController.cs
+++ Presentation/CarBook.WebApi/Controllers/TestimonialsController.cs
@@ -40,7 +40,7 @@ namespace CarBook.WebApi.Controllers
             await _mediator.Send(command);
             return Ok("Referans başarıyla güncellendi.");
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> RemoveTestimonial(int id)
         {
             await _mediator.Send(new RemoveTestimonialCommand(id));

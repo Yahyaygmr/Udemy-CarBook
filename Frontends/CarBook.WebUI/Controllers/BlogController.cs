6dc0b8f33b2135ac897bb8422266457c433e84c3
Yahyaygmr <yahyaygmr@gmail.com>
2024-05-18 13:08:39 +0300

    created comment on blogs function

--- Frontends/CarBook.WebUI/Controllers/BlogController.cs
+++ Frontends/CarBook.WebUI/Controllers/BlogController.cs
@@ -1,35 +1,51 @@
 ﻿using CarBook.Dto.BlogDtos;
+using CarBook.Dto.CommentDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace CarBook.WebUI.Controllers
 {
-	public class BlogController : Controller
-	{
-		private readonly IHttpClientFactory _httpClientFactory;
+    public class BlogController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
 
-		public BlogController(IHttpClientFactory httpClientFactory)
-		{
-			_httpClientFactory = httpClientFactory;
-		}
+        public BlogController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
 
-		public async Task<IActionResult> Index()
-		{
-			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.GetAsync("https://localhost:44386/api/Blogs/GetAllBlogsWithAuthorsList");
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44386/api/Blogs/GetAllBlogsWithAuthorsList");
 
-			if (responseMessage.IsSuccessStatusCode)
-			{
-				var jsonData = await responseMessage.Content.ReadAsStringAsync();
-				var values = JsonConvert.DeserializeObject<List<ResultAllBlogsWithAuthorDto>>(jsonData);
-				return View(values);
-			}
-			return View();
-		}
-		public async Task<IActionResult> BlogDetail(int id)
-		{
-			ViewBag.blogId = id;
-			return View();
-		}
-	}
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAllBlogsWithAuthorDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        public async Task<IActionResult> BlogDetail(int id)
+        {
+            ViewBag.blogId = id;
+            return View();
+        }
+
+        public async Task<IActionResult> AddComment(CreateCommentDto commentDto)
+        {
+            //commentDto.CreatedDate = DateTime.Now;
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(commentDto);
+            StringContent content = new StringContent(jsonData,Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44386/api/Comments", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("BlogDetail", "Blog", new { id = commentDto.BlogId });
+            }
+            return RedirectToAction("Index");
+        }
+    }
 }

4126c5d9130e48830aab9020e8bbc4a585b1aecb
Yahyaygmr <yahyaygmr@gmail.com>
2024-05-27 15:01:33 +0300

    created dashboard charts

---
+++ Frontends/CarBook.WebUI/Areas/Admin/ViewComponents/DashboardViewComponents/DashboardChart2ComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Areas.Admin.ViewComponents.DashboardViewComponents
+{
+    public class DashboardChart2ComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}

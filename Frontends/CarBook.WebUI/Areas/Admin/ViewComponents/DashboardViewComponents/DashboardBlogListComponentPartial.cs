199ec25ecf1941deccfd46d7f147cff06ed7f767
Yahyaygmr <yahyaygmr@gmail.com>
2024-05-27 13:13:16 +0300

    created dashboard statistics

---
+++ Frontends/CarBook.WebUI/Areas/Admin/ViewComponents/DashboardViewComponents/DashboardBlogListComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using CarBook.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Areas.Admin.ViewComponents.DashboardViewComponents
+{
+    public class DashboardBlogListComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DashboardBlogListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:44386/api/Blogs/GetAllBlogsWithAuthorsList");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAllBlogsWithAuthorDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ Frontends/CarBook.WebUI/Areas/Admin/ViewComponents/DashboardViewComponents/DashboardTopStatisticsComponentPartial.cs
@@ -0,0 +1,60 @@
+﻿using CarBook.Dto.StatisticsDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace CarBook.WebUI.Areas.Admin.ViewComponents.DashboardViewComponents
+{
+    public class DashboardTopStatisticsComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DashboardTopStatisticsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var client = _httpClientFactory.CreateClient();
+            Random random = new Random();
+
+
+            #region CarCount
+            var valueCarCount = GetStatisticAsync(client, "GetCarCount");
+            ViewBag.carCount = valueCarCount.Result.result.carCount;
+            ViewBag.carCountRandom = random.Next(0, 101);
+            #endregion
+
+            #region LocationCount
+            var valueLocationCount = GetStatisticAsync(client, "GetLocationCount");
+            ViewBag.locationCount = valueLocationCount.Result.result.locationCount;
+            ViewBag.locationCountRandom = random.Next(0, 101);
+            #endregion
+
+            #region AuthorCount
+            var valueAuthorCount = GetStatisticAsync(client, "GetAuthorCount");
+            ViewBag.authorCount = valueAuthorCount.Result.result.authorCount;
+            ViewBag.authorCountRandom = random.Next(0, 101);
+            #endregion
+
+            #region BrandCount
+            var valueBrandCount = GetStatisticAsync(client, "GetBrandCount");
+            ViewBag.brandCount = valueBrandCount.Result.result.brandCount;
+            ViewBag.brandCountRandom = random.Next(0, 101);
+            #endregion
+            return View();
+        }
+        public async Task<ResultStatisticsDto> GetStatisticAsync(HttpClient client, string endpoint)
+        {
+            var responseMessage = await client.GetAsync($"https://localhost:44386/api/Statistics/{endpoint}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
+                return values;
+            }
+            throw new Exception();
+        }
+    }
+}

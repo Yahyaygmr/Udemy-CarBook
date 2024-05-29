7befe56e4a6f3228efd29bd95337b6cca63503bb
Yahyaygmr <yahyaygmr@gmail.com>
2024-05-29 11:23:32 +0300

    committed

--- Frontends/CarBook.WebUI/Controllers/CarController.cs
+++ Frontends/CarBook.WebUI/Controllers/CarController.cs
@@ -27,5 +27,10 @@ namespace CarBook.WebUI.Controllers
             }
             return View();
         }
+        public async Task<IActionResult> CarDetail(int id)
+        {
+            ViewBag.carId = id;
+            return View();
+        }
     }
 }
---
+++ Frontends/CarBook.WebUI/ViewComponents/CarDetailViewComponents/CarDetailMainCarFeatureComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using CarBook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.CarDetailViewComponents
+{
+    public class CarDetailMainCarFeatureComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CarDetailMainCarFeatureComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:44386/api/Cars/{id}");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultCarWithBrandDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}

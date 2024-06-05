61e14d214ffaf72c6400379d815bdb1eec747c87
Yahyaygmr <yahyaygmr@gmail.com>
2024-06-05 23:42:58 +0300

    committed

---
+++ Frontends/CarBook.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,35 @@
+﻿using CarBook.Dto.RegisterDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RegisterController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateRegisterDto createRegisterDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createRegisterDto);
+            StringContent content = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMesssage = await client.PostAsync("https://localhost:44386/api/Register", content);
+            if (responseMesssage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index","Login");
+            }
+            return View();
+        }
+    }
+}
